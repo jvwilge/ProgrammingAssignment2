e0e46db64905f8e889ab27d04b46d5df138a7796
Jeroen van Wilgenburg <wilgenburg@gmail.com>
2016-03-12 11:21:14 +0100

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Provide two methods to be able to cache an inverse matrix calculation
+##
+## Step 1 is wrapping your matrix in a cacheMatrix list with the 
+## makeCacheMatrix function.
+##
+## Step 2 is retrieving the cachedMatrix via cacheSolve. cacheSolve will
+## calculate the inverse matrix the first time it's called. The successive
+## calls will return the cached calculation.
+##
+## Assumption: the matrix supplied is always invertible
+##
+## Comments based on https://google.github.io/styleguide/Rguide.xml#comments
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Wraps a matrix in a cacheMatrix object to provide caching of the inverse
+  # matrix calculation
+  #
+  # Args:
+  #    x: a matrix
+  #
+  # Returns:
+  #     The cacheMatrix object. This is a list with the functions : 
+  #     set: set the matrix
+  #     get: get the matrix
+  #     setsolve: set the inverse matrix (this will be the cached entry)
+  #     getsolve: gets the cached inverse matrix (return NULL when there's no 
+  #               entry)
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(mean) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # Search the cache for the inverse matrix of x. When the entry is not in the
+  # cache it's calculated, saved in the cache and returned.
+  #
+  # Args:
+  #     x: a matrix wrapped in a cacheMatrix object (which can be created with 
+  #     the makeCacheMatrix function)
+  #
+  # Returns:
+  #    The inverse matrix (a 'matrix' object, NOT wrapped in the cacheMatrix)
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
